46e98e1265b0cec533414614044215e2da4d7e66
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-08-31 14:48:51 +0300

    Converting to List Builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,52 +11,51 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 400,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions
-              .map((Transaction tx) => Card(
-                    elevation: 2,
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                              color: Colors.purpleAccent,
-                              border: Border.all(color: Colors.purple, width: 2)),
-                          padding: const EdgeInsets.all(10),
-                          child: Text("€${tx.amount}",
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.white)),
-                        ),
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                margin: const EdgeInsets.only(bottom: 4),
-                                child: Text(tx.title,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      color: Colors.purple,
-                                      fontSize: 16,
-                                    )),
-                              ),
-                              Text(DateFormat('d MMM yyyy').format(tx.date),
-                                  style: const TextStyle(
-                                    color: Colors.grey,
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 14,
-                                  )),
-                            ])
-                      ],
-                    ),
-                  ))
-              .toList(),
-        ),
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, i){
+          return  Card(
+            elevation: 2,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      color: Colors.purpleAccent,
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  padding: const EdgeInsets.all(10),
+                  child: Text("€${transactions[i].amount}",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.white)),
+                ),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 4),
+                        child: Text(transactions[i].title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.purple,
+                              fontSize: 16,
+                            )),
+                      ),
+                      Text(DateFormat('d MMM yyyy').format(transactions[i].date),
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                          )),
+                    ])
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
       ),
     );
   }
