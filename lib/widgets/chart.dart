5b5732d99682a9c0bd4cd45cf79bcfe456c6efab
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-09-01 14:44:55 +0300

    Adding chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,37 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:personal_expenses_app/models/transaction.dart';
+import 'package:personal_expenses_app/widgets/chart_bar.dart';
 
 class Chart extends StatelessWidget {
-
   final List<Transaction> recentTransactions;
+
   const Chart(this.recentTransactions, {Key? key}) : super(key: key);
 
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
-      final weekday = DateTime.now().subtract(Duration(days: index),);
+      final weekday = DateTime.now().subtract(
+        Duration(days: index),
+      );
       var amtSum = 0.0;
-      for(var i = 0 ; i< recentTransactions.length; i++){
+      for (var i = 0; i < recentTransactions.length; i++) {
         final Transaction t = recentTransactions[i];
-        if (t.date.day == weekday.day
-            && t.date.month == weekday.month
-            && t.date.year == weekday.year){
-              amtSum += t.amount;
+        if (t.date.day == weekday.day &&
+            t.date.month == weekday.month &&
+            t.date.year == weekday.year) {
+          amtSum += t.amount;
         }
       }
-      return {'day': DateFormat.E().format(weekday).substring(0,1), 'amount' : amtSum };
+      return {
+        'day': DateFormat.E().format(weekday).substring(0, 1),
+        'amount': amtSum
+      };
+    });
+  }
+
+  double get maxSpending {
+    return groupedTransactionValues.fold(0.0, (acc, trx) {
+      return acc + (trx['amount'] as double);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 6,
-      margin: const EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((t) =>
-            Text("${t['day']} : ${t['amount']} ")).toList()
-      )
-    );
+        elevation: 6,
+        margin: const EdgeInsets.all(20),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: groupedTransactionValues
+                  .map((t) => Flexible(
+                      fit: FlexFit.tight,
+                      child: ChartBar(
+                          t['day'].toString(),
+                          (t['amount'] as double),
+                          maxSpending == 0.0
+                              ? 0.0
+                              : (t['amount'] as double) / maxSpending)))
+                  .toList()),
+        ));
   }
 }
--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-
   final String label;
   final double spendingAmount;
   final double spendingOfTotal;
 
-  const ChartBar(
-      this.label,
-      this.spendingAmount,
-      this.spendingOfTotal,
-      {Key? key}) : super(key: key);
+  const ChartBar(this.label, this.spendingAmount, this.spendingOfTotal,
+      {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        FittedBox(child: Text("€${spendingAmount.toStringAsFixed(0)}")),
+        const SizedBox(height: 10),
+        SizedBox(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: const Color.fromRGBO(220, 220, 200, 1),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+              FractionallySizedBox(
+                  heightFactor: spendingOfTotal,
+                  child: Container(
+                      decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(16),
+                  ))),
+            ],
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(label)
+      ],
+    );
   }
 }
